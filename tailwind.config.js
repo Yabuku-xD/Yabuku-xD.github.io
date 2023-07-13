ca4c687bd39b4de2f9f7e8eb2345c8d59f4a20ea
umraz <shyamalankannan@gmail.com>
2023-07-13 13:11:05 +0530

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./portfolio/*.{html,js}"],
+  content: ["./Yabuku-xD.github.io/*.{html,js}"],
   theme: {
     extend: {},
   },
